bf9c442aae89e782e419d928c02e923b32fa0809
raph <rap2hpoutre@users.noreply.github.com>
2016-02-08 11:16:44 +0100

    Fix "Class web does not exist"
    Fix https://github.com/GeneaLabs/laravel-caffeine/issues/16

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,6 +2,10 @@
 
 use GeneaLabs\LaravelCaffeine\Http\Controllers\LaravelCaffeineController;
 
-Route::group(['middleware' => ['web']], function () {
+if (isset(Route::getMiddleware()['web'])) {
+    Route::group(['middleware' => ['web']], function () {
+        Route::get('genealabs/laravel-caffeine/drip', LaravelCaffeineController::class . '@drip');
+    });
+} else {
     Route::get('genealabs/laravel-caffeine/drip', LaravelCaffeineController::class . '@drip');
-});
+}
