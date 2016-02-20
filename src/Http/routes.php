0cc86c7c95efe88c73f98a09cffebce682103595
Mike Bronner <mike@genealabs.com>
2016-02-19 19:42:12 -0800

    Add method to detect web middleware

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -2,10 +2,23 @@
 
 use GeneaLabs\LaravelCaffeine\Http\Controllers\LaravelCaffeineController;
 
-if (isset(Route::getMiddleware()['web'])) {
+if (hasWebMiddleware()) {
     Route::group(['middleware' => ['web']], function () {
         Route::get('genealabs/laravel-caffeine/drip', LaravelCaffeineController::class . '@drip');
     });
 } else {
     Route::get('genealabs/laravel-caffeine/drip', LaravelCaffeineController::class . '@drip');
+}
+
+function hasWebMiddleware()
+{
+    $routes = Route::getRoutes()->getRoutes();
+
+    foreach ($routes as $route) {
+        if (in_array('web', $route->getAction()['middleware'])) {
+            return true;
+        }
+    }
+
+    return false;
 }
