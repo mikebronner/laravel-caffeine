deb0c287e915319bfdea9832032640423366301e
Christopher <git@idragonmail.com>
2016-06-18 14:27:09 -0500

    Fixed the Undefined variable: dripRoute

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -6,7 +6,7 @@ use GeneaLabs\LaravelCaffeine\Http\Controllers\LaravelCaffeineController;
 $dripRoute = config('genealabs-laravel-caffeine.route', 'genealabs/laravel-caffeine/drip');
 
 if ((new Helper())->routeHasMiddlewareGroup('web')) {
-    Route::group(['middleware' => ['web']], function () {
+    Route::group(['middleware' => ['web']], function () use ($dripRoute) {
         Route::get($dripRoute, LaravelCaffeineController::class.'@drip');
     });
 } else {
