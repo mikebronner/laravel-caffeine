1aaf107784e173cdad1fe2a47908fdc1aae12ead
Mike Bronner <mike@genealabs.com>
2016-09-24 15:48:08 -0700

    Tweak service provider to work correctly

--- src/Providers/LaravelCaffeineService.php
+++ src/Providers/LaravelCaffeineService.php
@@ -1,7 +1,6 @@
 <?php namespace GeneaLabs\LaravelCaffeine\Providers;
 
 use GeneaLabs\LaravelCaffeine\Http\Middleware\LaravelCaffeineDripMiddleware;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class LaravelCaffeineService extends ServiceProvider
@@ -10,15 +9,11 @@ class LaravelCaffeineService extends ServiceProvider
 
     public function boot()
     {
-        $router = new Route();
-
-        if (! $this->app->routesAreCached()) {
-            $router::group([
-                'middleware' => $this->middlewareGroupIfExists('web'),
-            ], function () {
+        app('router')->group($this->middlewareGroupExists('web')
+            ? ['middleware' => 'web']
+            : [], function () {
                 require __DIR__ . '/../../routes/web.php';
             });
-        }
 
         $this->publishes([__DIR__ . '/../../config/genealabs-laravel-caffeine.php' => config_path('genealabs-laravel-caffeine.php')], 'genealabs-laravel-caffeine');
     }
@@ -26,28 +21,27 @@ class LaravelCaffeineService extends ServiceProvider
     public function register()
     {
         $this->mergeConfigFrom(__DIR__ . '/../../config/genealabs-laravel-caffeine.php', 'genealabs-laravel-caffeine');
-        app('Illuminate\Contracts\Http\Kernel')->pushMiddleware(LaravelCaffeineDripMiddleware::class);
+
+        app('Illuminate\Contracts\Http\Kernel')->pushMiddleware('\GeneaLabs\LaravelCaffeine\Http\Middleware\LaravelCaffeineDripMiddleware');
     }
 
-    /**
-     * @return array
-     */
-    public function provides()
+    public function provides() : array
     {
         return ['genealabs-laravel-caffeine'];
     }
 
-    private function middlewareGroupIfExists(string $group) : string
+    private function middlewareGroupExists(string $group) : bool
     {
-        $routes = collect(Route::getRoutes()->getRoutes());
+        $routes = collect(app('router')->getRoutes()->getRoutes());
 
         return $routes->reduce(function ($carry, $route) use ($group) {
+            $carry = $carry ?? false;
             $actions = (array) $route->getAction();
 
             if (array_key_exists('middleware', $actions)
                 && in_array($group, (array) $actions['middleware'])
             ) {
-                return $group;
+                return true;
             }
 
             return $carry;
