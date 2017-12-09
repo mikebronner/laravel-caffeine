154d9b95a6515baa622783ca423c22eacc1c7782
ItaloBC <ItaloBC@users.noreply.github.com>
2017-12-09 11:47:40 -0300

    Changed the old drip to a sensible 10 seconds

--- config/genealabs-laravel-caffeine.php
+++ config/genealabs-laravel-caffeine.php
@@ -4,6 +4,6 @@ return [
     'dripIntervalInMilliSeconds' => 300000,         // Drip every 5 minutes
     'domain' => null,                               // defaults to url('/')
     'route' => 'genealabs/laravel-caffeine/drip',   // Can be customized
-    'thresholdDifference' => 60000,                 // When the drip will be considered old to reload the page
+    'thresholdDifference' => 10000,                 // When the drip will be considered old to reload the page
     'checkLastDripInterval' => 2000                 // How often we will check if the drip is old
 ];
