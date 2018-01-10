7601045dd58432b92ef6db77f66ef472d538009a
Mike Bronner <mike@genealabs.com>
2018-01-09 18:15:31 -0800

    Clean up route file

--- tests/routes/web.php
+++ tests/routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
-use GeneaLabs\LaravelCaffeine\Tests\Http\Controllers\Test;
-
 Route::group([
     'middleware' => ['web'],
     'as' => 'genealabs-laravel-caffeine.',
-    'prefix' => 'tests'
+    'prefix' => 'tests',
+    'namespace' => 'GeneaLabs\LaravelCaffeine\Tests\Http\Controllers',
 ], function () {
-    Route::any('form', Test::class . '@drippedForm')
+    Route::any('form', 'Test@drippedForm')
         ->name('tests.form');
-    Route::any('dripped-form', Test::class . '@drippedForm');
-    Route::any('expiring-form', Test::class . '@expiredForm');
-    Route::any('disabled-page', Test::class . '@disabledPage')
+    Route::any('dripped-form', 'Test@drippedForm');
+    Route::any('expiring-form', 'Test@expiredForm');
+    Route::any('disabled-page', 'Test@disabledPage')
         ->name('tests.disabled-page');
-    Route::any('null-response', Test::class . '@nullResponse')
+    Route::any('null-response', 'Test@nullResponse')
         ->name('tests.null-response');
-    Route::any('route-middleware', Test::class . '@drippedForm')
+    Route::any('route-middleware', 'Test@drippedForm')
         ->name('tests.route-middleware')
         ->middleware('caffeinated');
 });
