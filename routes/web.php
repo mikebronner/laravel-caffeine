c0ee2869cd3c2927ed05f1f8459e08f90f5efbe3
ItaloBC <ItaloBC@users.noreply.github.com>
2018-04-10 20:57:41 -0300

    Fixes caffeine not hitting the web middleware

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use GeneaLabs\LaravelCaffeine\Http\Controllers\Drip;
 
 $dripRoute = config('genealabs-laravel-caffeine.route', 'genealabs/laravel-caffeine/drip');
 
-Route::get($dripRoute, Drip::class.'@drip');
+Route::get($dripRoute, Drip::class.'@drip')->middleware(['web']);
